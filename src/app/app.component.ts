83b54e3091c413505c7f8093544c46a6116cfedc
Othior <splingaire.marvyn@gmail.com>
2021-03-01 14:20:03 +0100

    change bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { element } from "./Model/Personne";
 import { Formation ,list_etudes , list_experiences } from "./Model/Formation";
-import { Competence ,list_backend , list_frontend , list_langageDb , list_reseaux } from "./Model/Competence";
 
 @Component({
   selector: 'app-root',
