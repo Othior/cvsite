d67de3eccc859312ada41ec11978bf21ae8bac80
Othior <splingaire.marvyn@gmail.com>
2021-04-02 14:08:38 +0200

    fix detail scroll

--- src/app/service/service-all-page.service.ts
+++ src/app/service/service-all-page.service.ts
@@ -14,12 +14,11 @@ export class ServiceAllPageService {
   reloadPage(){
     if(this.router.url !== JSON.parse(localStorage.getItem('routeOld'))){
       localStorage.setItem('routeOld',JSON.stringify(this.router.url));
+      window.scrollTo(0,0);
       location.reload();
     }else if(this.router.url === JSON.parse(localStorage.getItem('routeOld')) ){
       let check = document.querySelector("#checkList").classList;
       check.remove('active');
-      console.log(check)
-      
     }
   }
 
